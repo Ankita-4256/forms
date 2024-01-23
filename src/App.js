eda21ccd631cd003b576d347711762a0746d4321
Ankita <ankitasinghr45@gmail.com>
2024-01-23 21:54:16 +0530

    1/23/24

--- src/App.js
+++ src/App.js
@@ -2,38 +2,91 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-const App =()=>{
+const App = () => {
 
-  const [myName,setName]=useState('');
+  // const [myName, setName] = useState('');
 
-  const [newName,UpdateName]=useState('');
+  // const [newName, UpdateName] = useState('');
 
-  const [country,newCountry]=useState('');
+  // const [country, newCountry] = useState('');
 
-  const [countryName,updateCountry]=useState('');
+  // const [countryName, updateCountry] = useState('');
 
-  const Change =(event)=>{
-    UpdateName(event.target.value);
-  }
-  const ChangeCountry =(event)=>{
-    newCountry(event.target.value);
-  }
+  // const Change = (event) => {
+  //   UpdateName(event.target.value);
+  // }
+  // const ChangeCountry = (event) => {
+  //   newCountry(event.target.value);
+  // }
 
-  const SubmitForm =(event)=>{
+  // const SubmitForm = (event) => {
+  //   event.preventDefault();
+  //   setName(newName);
+  //   updateCountry(country)
+  // }
+  const [myName,setMyName]=useState('');
+  const formFilled = (event) => {
     event.preventDefault();
-    setName(newName);
-    updateCountry(country)
+    setMyName(fullName.fName+' '+fullName.lName+' '+fullName.email)
   }
-  return(
+
+  const [fullName, setFullName] = useState({
+    fName: '',
+    lName: '',
+    email:''
+  })
+
+  const changeValue = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFullName((preValue) => {
+      console.log(preValue);
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: preValue.lName,
+          email:preValue.email
+        }
+      } else if (name === "lName") {
+        return {
+          fName: preValue.fName,
+          lName: value,
+          email:preValue.email
+        }
+      }else{
+        return{
+          fName:preValue.fName,
+          lName: preValue.lName,
+          email:value
+        }
+      }
+    })
+  }
+  return (
     <>
-    <form onSubmit={SubmitForm}>
-    <div>
-      <h1 style={{textTransform:"uppercase"}}>Hello {myName} {countryName?`from ${countryName}`:``}</h1>
-      <input type='text' placeholder='Enter Your Name' onChange={Change} value={newName}></input>
-      <br/>
-      <input type='text' placeholder='Enter Your Country' onChange={ChangeCountry} value={country}></input>
-      <button type='submit'>Submit</button>
-      </div>
+      {/* <form onSubmit={SubmitForm}>
+        <div>
+          <h1 style={{ textTransform: "uppercase" }}>Hello {myName} {countryName ? `from ${countryName}` : ``}</h1>
+          <input type='text' placeholder='Enter Your Name' onChange={Change} value={newName}></input>
+          <br />
+          <input type='text' placeholder='Enter Your Country' onChange={ChangeCountry} value={country}></input>
+          <button type='submit'>Submit</button>
+        </div>
+      </form> */}
+
+      <form onSubmit={formFilled}>
+        <div>
+        <h1 style={{ textTransform: "uppercase" }}>Hello {myName}</h1>
+          <input type='text' placeholder='Enter Your First Name' name='fName' onChange={changeValue}
+            value={fullName.fName}></input>
+          <br />
+          <input type='text' placeholder='Enter Your Last Name' name='lName' onChange={changeValue}
+            value={fullName.lName}></input>
+            <br/>
+            <input type='email' placeholder='Enter Your Email' name='email' onChange={changeValue}
+            value={fullName.email}></input>
+          <button type='submit'>Submit</button>
+        </div>
       </form>
     </>
   )
